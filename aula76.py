77ea35e40dc58c23711d9875e80881190e4f6b04
Leonardo Prado Suzin <leonardopradosuzin@gmail.com>
2025-02-03 15:24:43 -0300

    Parte1 dict

---
+++ aula76.py
@@ -0,0 +1,34 @@
+"""        
+Dicionarios em python (tipo dict)
+DIcionarios são estruturas de dados do tipo
+par de 'chave' e 'valor'.
+Chaves podem ser consideradas com um 'indice'
+que vimos na lista e podem ser de tipos imutáveis
+(qualquer tipo: str, int, float, bool, tuple, etc)
+E valores podem ser de qualquer tipo, incluindo
+outros dicionários.
+Usamos chaves - {} - ou a classe dict() para criar dicionários.
+Imutaveis: str, int, float, bool, tuple
+Mutaveis: dict, list
+"""
+# pessoa = dict(nome='Luiz Otávio', sobrenome='Miranda', idade=28, altura=1.8)
+
+# pessoa = {
+#     'nome': 'Luiz Otávio',
+#     'sobrenome': 'Miranda',
+#     'idade': 28,
+#     'altura': 1.8,
+#     'enderecos': [
+#         {'rua': 'tal tal', 'numero': 123},
+#         {'rua': 'outra rua', 'numero': 321}
+#     ]
+# }
+
+# print(pessoa, type(pessoa))
+
+# print(pessoa['nome'])
+# print(pessoa['sobrenome'])
+
+# for chave in pessoa:
+#     print(chave, pessoa[chave])
+
